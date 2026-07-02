ec17c363adbf2c13dd06f72dd6eeec6b67d131b1
Yogesh Late <y.late@worldergroup.com>
2026-07-02 15:57:53 +0530

    setup: nativewind(tailwind css for react native)

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 
 const SignInScreen = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View className="flex-1 justify-center items-center">
       <Text>SignInScreen</Text>
     </View>
   );
--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
 import { MMKV_KEYS } from "@/constants";
 import { getItem } from "@/utils";
 import { Redirect } from "expo-router";
+import "../global.css";
 
 export default function Index() {
   const isSignedIn: boolean = getItem<boolean>(MMKV_KEYS.IS_SIGNED_IN) || false;
